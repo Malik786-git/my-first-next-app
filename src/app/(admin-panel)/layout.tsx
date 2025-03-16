8a4f8fbed2b08176c101c1472c8a18ea5e1fad0e
Malik786-git <malikmuhammadjahangir786@gmail.com>
2025-03-16 18:03:44 +0500

    remove

--- src/app/(admin-panel)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ILayoutPage } from "../types/layout";
-import Header from "../components/header/header";
-
-export default function ServiceLayout({ children }: ILayoutPage) {
-    return <main>
-        <nav><Header></Header></nav>
-        <div>{children}</div>
-    </main>
-}
--- src/app/types/layout.ts
+++
@@ -1,3 +0,0 @@
-export interface ILayoutPage {
-    children: React.ReactNode;
-}
