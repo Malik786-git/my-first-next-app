028e56ddf90f87a489142e50c8bc7b42e27e2555
Malik786-git <malikmuhammadjahangir786@gmail.com>
2025-04-13 17:05:03 +0500

    sfsadf

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,3 +1,32 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Devtrio E Shopping Store",
+    description: "Explore best deals and offer on Devtrio E shopping store",
+    authors: [{ name: "Devtrio", url: "devtrio.pro" }], // for blogs posts
+    openGraph: {
+        title: "Graph Devtrio E Shopping Store",
+        description: "Graph Explore best deals and offer on Devtrio E shopping store",
+        url: "https://my-first-next-app-one-gamma.vercel.app/products",
+        siteName: "Graph Devtrio",
+        images: [
+            {
+                url: "/shopping.jpeg",
+                alt: "Devtrio E Shopping Store",
+            },
+        ],
+    },
+    twitter: {
+        images: [
+            {
+                url: "/shopping.jpeg",
+                alt: "Devtrio E Shopping Store",
+            },
+        ],
+    }
+}
+
+
 export default function ProductListingLayout({
     children,
 }: Readonly<{
