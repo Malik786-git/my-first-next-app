ac1cab362a7e0666f48bc86de9413428c8b49108
Malik786-git <malikmuhammadjahangir786@gmail.com>
2025-05-11 18:08:01 +0500

    integrate api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
           <Link href={'/products'}>Products</Link>
           <Link href={'/checkout?token=sdfsadf34ewfef234defe3242srsdfsd'}>Checkout</Link>
           <Link href={'/merchant'}>Merchant</Link>
+          <Link href={'/blogs'}>Blogs</Link>
         </div>
         {children}
       </body>
