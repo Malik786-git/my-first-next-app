90db2d0296019a06675914faea81f0c4e949c08b
Malik786-git <malikmuhammadjahangir786@gmail.com>
2025-05-18 20:39:34 +0500

    fix server

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,11 +1,29 @@
+import connectToDatabase from "@/config/db";
 import { Constants } from "../../../../constants";
 import BlogDetail from "./blog-detail";
+import Blog from "@/app/api/schema/blog.schema";
+// export async function generateStaticParams() {
+//     // const blogs = await fetch(`${Constants.PROD_BASE_URL}/api/blog/all`, { cache: "force-cache" }).then((res) => res.json());
+//     const blogs = await fetch(`${Constants.PROD_BASE_URL}/api/blog/all`, { cache: "force-cache" }).then((res) => res.json());
+//     return blogs?.data?.map((b: any) => ({
+//         id: b._id,
+//     })) ?? [];
+// }
+
 export async function generateStaticParams() {
-    const blogs = await fetch(`${Constants.PROD_BASE_URL}/api/blog/all`, { cache: "force-cache" }).then((res) => res.json());
-    return blogs?.data?.map((b: any) => ({
-        id: b._id,
-    })) ?? [];
+    try {
+        await connectToDatabase();
+        const blogs = await Blog.find({}, { _id: 1 }); // Only fetch IDs
+
+        return blogs.map((blog) => ({
+            id: blog._id.toString(),
+        }));
+    } catch (error) {
+        console.error("Error in generateStaticParams:", error);
+        return [];
+    }
 }
+
 
 interface IPageParams {
     params: Promise<{
