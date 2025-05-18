bdcac190f24e929d17da67713916d66c36081f19
Malik786-git <malikmuhammadjahangir786@gmail.com>
2025-05-18 20:24:21 +0500

    add fix

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,15 +1,9 @@
-import { Constants } from "../../../../constants";
 import BlogDetail from "./blog-detail";
-
 export async function generateStaticParams() {
-    if (Constants.PROD_EVN) {
-        const blogs = await fetch(`/api/blog/all`, { cache: "force-cache" }).then((res) => res.json());
+ const blogs = await fetch(`http://localhost:3000/api/blog/all`, { cache: "force-cache" }).then((res) => res.json());
         return blogs?.data?.map((b: any) => ({
             id: b._id,
         })) ?? []
-    }
-
-    return []
 }
 
 interface IPageParams {
