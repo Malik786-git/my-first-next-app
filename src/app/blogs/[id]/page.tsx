52be5bbd2ceccd6bb02c9a977486d7eaa6b4adff
Malik786-git <malikmuhammadjahangir786@gmail.com>
2025-05-18 20:30:24 +0500

    adfsdf

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,9 +1,10 @@
+import { Constants } from "../../../../constants";
 import BlogDetail from "./blog-detail";
 export async function generateStaticParams() {
- const blogs = await fetch(`http://localhost:3000/api/blog/all`, { cache: "force-cache" }).then((res) => res.json());
-        return blogs?.data?.map((b: any) => ({
-            id: b._id,
-        })) ?? []
+    const blogs = await fetch(`${Constants.PROD_BASE_URL}/api/blog/all`, { cache: "force-cache" }).then((res) => res.json());
+    return blogs?.data?.map((b: any) => ({
+        id: b._id,
+    })) ?? [];
 }
 
 interface IPageParams {
