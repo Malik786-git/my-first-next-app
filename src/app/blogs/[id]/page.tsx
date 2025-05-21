f579593e8d286ea0307ea41232c4c56304956619
Malik786-git <malikmuhammadjahangir786@gmail.com>
2025-05-22 03:01:36 +0500

    asdfsadf

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,31 +1,32 @@
-import connectToDatabase from "@/config/db";
-import { Constants } from "../../../../constants";
-import BlogDetail from "./blog-detail";
-import Blog from "@/app/api/schema/blog.schema";
-import { getAllBlogIds } from "@/app/server/all-blogs";
-// For third party apis service
-// export async function generateStaticParams() {
-//     // const blogs = await fetch(`${Constants.PROD_BASE_URL}/api/blog/all`, { cache: "force-cache" }).then((res) => res.json());
-//     const blogs = await fetch(`${Constants.PROD_BASE_URL}/api/blog/all`, { cache: "force-cache" }).then((res) => res.json());
-//     return blogs?.data?.map((b: any) => ({
-//         id: b._id,
-//     })) ?? [];
-// }
+import { fetchAllBlogQuery } from "@/app/action/blogs/all-blog.query";
+import BlogDetail from "./blog-details";
 
-// For next js apis routes
+
+
 export async function generateStaticParams() {
-    const blogs = await getAllBlogIds();
-    return blogs.map((blog) => ({ id: blog._id.toString() }));
+    const blogs: any = await fetchAllBlogQuery()
+    return blogs.map((blog: any) => ({
+        id: blog._id.toString()
+    })) ?? [];
 }
 
 
-interface IPageParams {
+// if we use separate backend service... like java python php etc.. node js
+// export async function generateStaticParams() {
+//     const blogs: any = await fetch(`https://jsonplaceholder.typicode.com/todos`).then((res) => res.json())
+//     return blogs.slice(0, 5).map((blog: any) => ({
+//         id: blog.id.toString(),
+//     })) ?? []
+// }
+
+
+interface IPageProps {
     params: Promise<{
         id: string
     }>
 }
 
-export default async function ProductListing(props: IPageParams) {
-    const { id } = await props.params;
-    return <BlogDetail blogId={id}></BlogDetail>
+export default async function BlogPage(props: IPageProps) {
+    const { id } = await props.params
+    return <BlogDetail id={id}></BlogDetail>
 }
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -3,33 +3,29 @@
 import { useEffect } from "react";
 import useApiState from "../hooks/use-api-state";
 import { useRouter } from "next/navigation";
+
 interface IBlog {
-    _id: string;
-    title: string;
-    content: string;
+    title: string
+    content: string
+    _id: string
 }
-export default function ProductListing() {
-    const [blogs, setBlogs, callBlogApi, loading, error] = useApiState<IBlog[] | null>(null, true);
-    const router = useRouter()
-    async function getAllBlogs() {
-        callBlogApi(async function () {
-            const url = `/api/blog/all`
-            const res = await fetch(url);
-            const body = await res.json();
-            setBlogs(body.data);
-        })
-    }
-
+export default function BlogsPage() {
+    const [blogs, setBlogs, callBlogs, error, loading] = useApiState<null | IBlog[]>([]);
+    const router = useRouter();
     useEffect(() => {
-        getAllBlogs();
-    }, [])
+        async function fetchBlogs() {
+            callBlogs(async function () {
+                const res = await fetch('/api/blog/all');
+                const body = await res.json();
+                console.log(body, 'body');
+                setBlogs(body.data);
+            })
+        }
+        fetchBlogs()
+    }, []);
     return <main>
-        <h1 style={{ textAlign: 'center', marginBottom: 20 }}>All Blogs Page</h1>
-
-        <section style={{ display: 'flex', justifyContent: "center", flexDirection: "column", gap: 30 }}>
-            {loading && <p>Loading...</p>}
-            {!loading && error && <p>{error}</p>}
-            {!loading && blogs && blogs.map(p => <p onClick={() => router.push(`/blogs/${p._id}`)} style={{ textAlign: "center" }}>{p.title}</p>)}
-        </section>
+        <h1>Blogs</h1>
+        {!error && loading && <h1>loading...</h1>}
+        {!error && blogs && blogs.map(b => <h1 onClick={() => router.push(`/blogs/${b._id}`)}>{b?.title}</h1>)}
     </main>
 }
